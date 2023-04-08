17f8482718295b8e4e5b41ce01af2364ff1824f8
rzc <rzc1937986979@163.com>
2023-04-08 15:53:30 +0800

    updated the fomula for converting EbN0 to SNR

--- test_BlerVsEbn0.m
+++ test_BlerVsEbn0.m
@@ -2,10 +2,11 @@
 clear; clc; close all;
 %% 初始化
 L_word = 1024;
-L_info = L_word / 2;
+L_info = 512;
 info_index = getInfoIndex(L_word, L_info);
-EbN0_dBs = linspace(-1, 3, 10);
-sigma2s = 1 ./ 10 .^ (EbN0_dBs / 10);
+EbN0_dBs = linspace(0, 7, 8);
+% (SNR_linear) = (Eb/N0_linear) * (nBits=1) * (CodeRate) / (1/2) / (SampleRate=1)
+sigma2s = 1 ./ (10 .^ (EbN0_dBs / 10) * L_info / L_word * 2);
 %% 仿真多次取平均
 blers = zeros(size(EbN0_dBs));
 N_test = 1e4; test_step = 1e2;
